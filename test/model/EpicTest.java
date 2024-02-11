38c6cc9e268c94c2926137c21f3486eed2d24dd0
Vladimir-Surovtsev <kuhlnas@yandex.ru>
2024-02-11 13:26:44 +0300

    5 sprint 1.24 version

--- test/model/EpicTest.java
+++ test/model/EpicTest.java
@@ -5,15 +5,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EpicTest {
+    Epic epic = new Epic("name", "desc", TaskStatus.NEW);
+    Epic epicExpected = new Epic("name2", "desc2", TaskStatus.DONE);
 
     @Test
-    void setUp() {
-        Epic epic = new Epic("name", "desc", TaskStatus.NEW);
-        Epic epicExpected = new Epic("name2", "desc2", TaskStatus.DONE);
-
+    void checkEpicsAreEqualIfIdentifiersAreEqual() {
         assertEquals(epicExpected, epic, "эпики должны быть равны");
+    }
 
+    @Test
+    void checkEpicCannotBeSubtask() {
         epic.addSubTaskId(epic.getId());
-        assertEquals(epic.getSubTasksId().size(), 0,  "Epic нельзя добавить в самого себя в виде подзадачи");
+        assertEquals(epic.getSubTasksId().size(), 0, "Epic нельзя добавить в самого себя в виде подзадачи");
     }
 }
--- test/model/SubTaskTest.java
+++ test/model/SubTaskTest.java
@@ -8,9 +8,12 @@ class SubTaskTest {
     @Test
     public void setUp() {
         SubTask subTask = new SubTask("name", "desc", TaskStatus.NEW);
+        SubTask subTask2 = new SubTask("name2", "desc2", TaskStatus.IN_PROGRESS);
         subTask.setId(3);
         subTask.setEpicId(3);
         assertEquals(subTask.getEpicId(), 0, "Подзадачу нельзя назначить своим эпиком");
+        subTask2.setId(subTask.getId());
+        assertEquals(subTask, subTask2, "Подзадачи должны быть равны, если равны их идентификаторы");
     }
 
 }
