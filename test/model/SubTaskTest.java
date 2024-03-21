5353e632b5e5ee0a6dea2ae0faeb0cf945816b20
Vladimir-Surovtsev <kuhlnas@yandex.ru>
2024-03-22 02:17:17 +0300

    7sprint 1.0 version

--- test/model/SubTaskTest.java
+++ test/model/SubTaskTest.java
@@ -5,14 +5,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SubTaskTest {
-    private final SubTask subTask = new SubTask("name", "desc", Status.NEW, 3);
-    private final SubTask subTask2 = new SubTask("name2", "desc2", Status.IN_PROGRESS, 3);
-    @Test
-    public void checkSubtaskCannotBeOwnTask() {
-        subTask.setId(3);
-        subTask.setEpicId(3);
-        assertEquals(subTask.getEpicId(), 0, "Подзадачу нельзя назначить своим эпиком");
-    }
+    private final SubTask subTask = new SubTask("name", "desc", Status.NEW, 4);
+    private final SubTask subTask2 = new SubTask("name2", "desc2", Status.IN_PROGRESS, 4);
+
     @Test
     public void checkSubtasksAreEqualIfIdentifiersAreEqual() {
         subTask2.setId(subTask.getId());
--- test/service/InMemoryHistoryManagerTest.java
+++ test/service/InMemoryHistoryManagerTest.java
@@ -1,19 +1,20 @@
 package service;
 
+import model.Status;
 import model.Task;
-import model.TaskStatus;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class InMemoryHistoryManagerTest {
 
     @Test
     void add() {
         HistoryManager historyManager = new InMemoryHistoryManager();
-        historyManager.addTaskToHistory(new Task("Новая задача", "Описание цели задачи", TaskStatus.NEW));
+        historyManager.addTaskToHistory(new Task("Новая задача", "Описание цели задачи", Status.NEW));
         final List<Task> history = historyManager.getTasksFromHistory();
         assertNotNull(history, "История не пустая.");
         assertEquals(1, history.size(), "История не пустая.");
